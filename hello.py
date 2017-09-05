d31040f28d9c9d6f6e6b511e823bb1f71bc4c957
stephanieherrera70 <30415954+stephanieherrera70@users.noreply.github.com>
2017-09-05 14:54:00 -0400

    removed folder

--- hello.py
+++ hello.py
@@ -3,4 +3,22 @@ print('hello, world')
 print('hello, world')
 print('hi')
 
-2+2
+print(2+2)
+
+#name = input("Please enter your name: ")
+#print("hello,", name)
+
+message = 'I learned something cool today!'
+n = 100
+pi = 3.14 
+print(n + 100)
+
+first_name = Stephanie 
+last_name = Herrera
+fullname = first_name + last_name
+
+# print(Naah, na na nanana naah, nanana naah, hey Jude,)
+
+'Today is %2d-%02d.' % (9, 6)
+
+
---
+++ variable_demo.py
@@ -0,0 +1,7 @@
+print('hello, world!')
+print('Hey Jude', 'don\'t make it bad')
+print('The total number of overall medals in Rio 2016 is', 46 + 37 + 38)
+print('46 + 37 + 38 =', 46 + 37 + 38)
+
+username = input()
+print(username)
