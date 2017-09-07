9619970ad3e0ea1b643d17cd393fae31bd5eb655
stephanieherrera70 <30415954+stephanieherrera70@users.noreply.github.com>
2017-09-06 23:14:10 -0400

    updated calc.py

--- calc.py
+++ calc.py
@@ -1,106 +1,34 @@
-Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
->>> 5-6
--1
->>> 2+2
-4
->>> 2+-2
-0
->>> 2++@
-  File "<stdin>", line 1
-    2++@
-       ^
-SyntaxError: invalid syntax
->>> 2++2
-4
->>> -2+7
-5
->>> 02+08
-  File "<stdin>", line 1
-    02+08
-     ^
-SyntaxError: invalid token
->>> 2 9
-  File "<stdin>", line 1
-    2 9
-      ^
-SyntaxError: invalid syntax
->>> print('Hello, world!'
-...
-... print('Hello, World!')
-  File "<stdin>", line 3
-    print('Hello, World!')
-        ^
-SyntaxError: invalid syntax
->>> print('Hello, World!')
-Hello, World!
->>> print'Hello, World!'
-  File "<stdin>", line 1
-    print'Hello, World!'
-                       ^
-SyntaxError: invalid syntax
->>> 1 minute= 60
-  File "<stdin>", line 1
-    1 minute= 60
-           ^
-SyntaxError: invalid syntax
->>> 42 Minutes= 60*42
-  File "<stdin>", line 1
-    42 Minutes= 60*42
-             ^
-SyntaxError: invalid syntax
->>> Seconds=42
->>> 42 minutes + seconds
-  File "<stdin>", line 1
-    42 minutes + seconds
-             ^
-SyntaxError: invalid syntax
->>> x=60
->>> y= 60*42
->>> s=42
->>> y+s
-2562
->>> 1 minute= 60
-  File "<stdin>", line 1
-    1 minute= 60
-           ^
-SyntaxError: invalid syntax
->>> 42 Minutes= 60*42
-  File "<stdin>", line 1
-    42 Minutes= 60*42
-             ^
-SyntaxError: invalid syntax
->>> Seconds=42
->>> 42 minutes + seconds
-  File "<stdin>", line 1
-    42 minutes + seconds
-             ^
-SyntaxError: invalid syntax
->>> x=60
->>> y=x*42
->>> s=42
->>> y+s
-2562
->>> x=1.61
->>> 10 / 1.61
-6.211180124223602
->>> 2562
-2562
->>> 2562/6.211180124223602
-412.482
->>> 42.42/6.211180124223602
-6.829620000000001
->>> print('Hello, World!)
-  File "<stdin>", line 1
-    print('Hello, World!)
-                        ^
-SyntaxError: EOL while scanning string literal
->>> 42.7/6.211180124223602
-6.874700000000002
->>> 42.7/60
-0.7116666666666667
->>> 6.211180124223602/0.7116666666666667
-8.727653570337614
->>>
+#Session 1: Exercise 2
+x = 60
+y = x * 42
+s = 42
+print(y + s)
+#2562 seconds
+
+x = 1.61
+print(10 / 1.61)
+#6.211180 miles
+
+print(2562/6.211180)
+#412.482 seconds per mile
+
+print(42/60)
+42.7/6.211180
+#6.874700000000 minutes per mile
+
+print(42.7/60)
+#0.711666666 hours
+
+print(6.211180124/0.711666666666)
+#8.727653703376
+
+#Session 2: Exercise 1
+#Volume of sphere
+pi = 3.14
+print((4/3)*pi*5*5*5)
+#Total Wholesale cost for 60 copies
+print(24.95 * .40 + 3 + .75 *59)
+#What time do I get home for breakfast?
+print((8 * 1 + 15 / 60) + ((7 * 3) + 3 * (12 / 60)) + (8 + 15 / 60))
+#percentage of the increase 
+print(((89-82)/82) * 100 + 'xx.x%') 
