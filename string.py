25111524945856cb006cc44f9f38c7b47ff32c26
stephanieherrera70 <30415954+stephanieherrera70@users.noreply.github.com>
2017-10-03 03:04:25 -0400

    Exercise 3,4 : String

---
+++ string.py
@@ -0,0 +1,154 @@
+#team = 'New England Patriots'
+#letter = team [0]
+
+#print(letter)
+#print(team[len(team)-1])
+#print(team[-1])
+
+#index = 0 
+#for i in range(len(team)):
+#    print(team[i])
+
+#index = 0 
+#for i in range(len(team)):
+#    if team[i] != ' ':
+#    print(team[i])
+
+#for letter in team: 
+#    if letter.isalpha():
+#        print(letter)
+
+#prefixes = 'JKLMNOPQ'
+#suffix = 'ack'
+#for letter in prefixes:
+#    if letter == 'O' or letter == 'Q':
+#        print(letter + 'u' + suffix)
+#    else:
+#        print(letter + suffix)
+
+#team = 'New England Patriots'
+#print(team[0:11])
+#print(team[12:20])
+
+#print(team[ :11])
+#print(team[12:])
+
+#print(team[::-1])
+
+#print(team[::-2])
+
+#print(team[4:0])
+#print(team[::1])
+
+#team = 'New England Patriots'
+#team[12:20] = 'Seahwks' #you need to assign a new variable
+
+#print(team)
+#new_team = team[:12] + 'Beavers'
+#print(new_team)
+
+#def find(word, letter):
+ #   index=0 #why? 
+  #  while index < len(word):
+ #       if word[index] == letter:
+ #           return index 
+ #       index = index + 1 
+ #   return -1 
+
+#print(find(team, 'e'))
+
+#word = 'New England Patriots'
+#count = 0
+#for letter in word:
+#    if letter == 'a':
+#        count = count + 1
+#print(count)
+
+#Exercise # 2
+#def count(word,letter)
+#for x in word:
+#    if x == letter:
+#         count = count + 1
+#    return count 
+#print(count(word, 'a'))
+
+#number_of_vowels= count(team,'a') + count(team, 'e') + count(team, 'i')
+#+ count(team,'o') + count(team. 'u')
+
+#vowels = 'aeiou'
+#number_of_vowels = 0
+#for letter in vowels: 
+#    number_of_vowels += count(team, letter)
+
+
+#print(number_of vowels)
+
+#Exercise 3
+a = "WHY ISN\'T THIS WORKING!"
+print(a.lower())
+
+
+#split
+print('1,2,3'.split(','))
+
+print('1,2,3,4,5,6'.split(',', maxsplit=2))
+
+print('1,2,3,4,5,6'.split(',', maxsplit=3))
+
+#strip
+print('     old     '.strip())
+
+print('www.example.com'.strip('cmowz.'))
+
+print('jstejpjhajie'.strip('j', 4)) #why doesn't this work
+
+#replace
+print('I went to the store'.replace('went', 'attended'))
+
+print('Hello John'.replace('you','me'))
+
+#Exerise 4
+<htm><body><table>
+<tr><td>bananas</td><td>52
+</td></tr>
+<tr><td>rice</td><td>35
+</td></tr>
+<tr><td>paprika</td><td>72
+</td></tr>
+<tr><td>potato chips</td><td>78
+</td></tr>
+</table></body></html>
+
+ def store(cost)
+    """ Print items:
+            bananas : $52
+            rice : $35
+            paprika : $72
+            potato chips $78
+            -----
+            Total: $237
+        """
+
+    count = 0
+        for letter in banana:
+    print(ord(str(letter))-96) 
+
+    count = 0
+        for letter in rice:
+    print(ord(str(letter))-96)
+
+    count = 0 
+        for letter in 'potatoe chips'
+     print(ord(str(letter))-96)
+
+        count = 0 
+        for letter in 
+     print(ord(str(letter))-96)
+
+
+#lost
+
+
+
+
+
